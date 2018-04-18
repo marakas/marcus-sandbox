c21070c0ddf124fd9705a315514f59dfd5e33a99
Marcus <marcus.saw@rakuten.com>
2018-04-18 18:32:08 +0900

    test

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 var path = require("path");
 const port = process.env.port || 3000;
 
-//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'))
 //app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
 
 app.listen(port, () => console.log('Example app listening on port 80!'))
